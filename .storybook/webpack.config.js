456904aec8b3a84416f84aadc0079c02c7fcdfee
janumedia@yahoo.com.sg <janumedia@gmail.com>
2019-01-04 10:23:58 +0800

    addon-storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,20 @@
 const path = require("path");
 
 module.exports = (baseConfig, env, defaultConfig) => {
+    //add addon-storysource
+    defaultConfig.module.rules.push({
+        test: [/\.stories\.js$/, /index\.js$/],
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        include: [path.resolve(__dirname, '../src')],
+        enforce: 'pre',
+    });
+    // allow SCSS
     defaultConfig.module.rules.push({
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"],
         include: path.resolve(__dirname, "../")
     });
+    // setup URL Alias
     defaultConfig.resolve.alias = {
         ...defaultConfig.resolve.alias,
         "@": path.resolve(__dirname, "../src")
