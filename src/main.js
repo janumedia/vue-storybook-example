d525da58bbe7601a56c0411ce68a802de9f15e7a
janumedia@yahoo.com.sg <janumedia@gmail.com>
2019-01-06 09:48:12 +0800

    add import async components

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ Vue.config.productionTip = false
 //register layouts
 Vue.component('default-layout', DefaultLayout);
 
+//register async components
+Vue.component('custom-button', () => import(/* webpackChunkName: "custom-button" */ '@/components/core/Button'));
+Vue.component('input-text', () => import(/* webpackChunkName: "input-text" */ '@/components/core/Input'));
+Vue.component('checkbox', () => import(/* webpackChunkName: "checkbox" */ '@/components/core/Checkbox'));
+Vue.component('radio-button', () => import(/* webpackChunkName: "radio-button" */ '@/components/core/RadioButton'));
+Vue.component('switch-button', () => import(/* webpackChunkName: "switch-button" */ '@/components/core/SwitchButton'));
+
 new Vue({
   router,
   render: h => h(App),
